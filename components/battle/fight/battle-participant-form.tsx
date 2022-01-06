3f602dce2d1c1378b73b9da7c8e2cff583f227d9
vdupre <vdupre@lafourchette.com>
2022-01-06 22:08:38 +0100

    feat(battle): add state to hide part of the participant card

--- components/battle/fight/battle-participant-form.tsx
+++ components/battle/fight/battle-participant-form.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   BattleParticipant,
   CONDITIONS,
@@ -17,13 +17,25 @@ export const BattleParticipantForm: React.FC<BattleParticipantFormProps> = ({
   const formRef =
     useRef<HTMLFormElement>() as React.MutableRefObject<HTMLFormElement>;
 
+  // states
+  const [isFormDisplayed, setIsFormDisplayed] =
+    useState<boolean>(formDisplayed);
+  const [areConditionsDisplayed, setAreConditionsDisplayed] =
+    useState<boolean>(false);
+
   // handlers
-  // const onHpChanged = () => {};
+  const handleParticipantInfoClicked = () =>
+    setIsFormDisplayed(!isFormDisplayed);
+  const handleConditionsHeaderClicked = () =>
+    setAreConditionsDisplayed(!areConditionsDisplayed);
 
   return (
     <div className="border p-2 rounded-md">
       <div className="mb-2">
-        <div className="flex flex-row space-x-2 items-center">
+        <div
+          className="flex flex-row space-x-2 items-center cursor-pointer"
+          onClick={handleParticipantInfoClicked}
+        >
           <div className="font-semibold text-lg p-0">
             {battleParticipant.name}
           </div>
@@ -32,7 +44,7 @@ export const BattleParticipantForm: React.FC<BattleParticipantFormProps> = ({
           </div>
         </div>
       </div>
-      {formDisplayed && (
+      {isFormDisplayed && (
         <form ref={formRef}>
           {/* damage & healing */}
           <div className="mb-2">
@@ -68,22 +80,29 @@ export const BattleParticipantForm: React.FC<BattleParticipantFormProps> = ({
 
           {/* conditions */}
           <div className="mb-2">
-            <div>conditions</div>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-              {Object.values(CONDITIONS).map((condition) => (
-                <div key={`condition-${condition}`}>
-                  <label className="py-1 block">
-                    <input
-                      type="checkbox"
-                      name="condition"
-                      value={condition}
-                      className="mr-2"
-                    />
-                    {condition}
-                  </label>
-                </div>
-              ))}
+            <div
+              className="cursor-pointer"
+              onClick={handleConditionsHeaderClicked}
+            >
+              conditions {!areConditionsDisplayed && <span>⬇️</span>}
             </div>
+            {areConditionsDisplayed && (
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {Object.values(CONDITIONS).map((condition) => (
+                  <div key={`condition-${condition}`}>
+                    <label className="py-1 block">
+                      <input
+                        type="checkbox"
+                        name="condition"
+                        value={condition}
+                        className="mr-2"
+                      />
+                      {condition}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
           <div className="text-right">
             <PrimaryButton submit>Validate</PrimaryButton>
